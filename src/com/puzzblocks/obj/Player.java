0b418fb1ce11406af747d8db8ba8e66c284c14b9
Cin316 <joe13reich@gmail.com>
2013-06-11 10:31:49 -0400

    Added onPlayerWalk hook to Block.

--- src/com/puzzblocks/obj/Player.java
+++ src/com/puzzblocks/obj/Player.java
@@ -6,6 +6,9 @@ import com.utilis.game.obj.*;
 
 public class Player extends ScrollingEntity {
 	
+	protected float walkSpeed = 1.0F;
+	protected boolean canJump = true;
+	
 	public Player(Image i) {
 		super(i);
 	}
@@ -17,6 +20,20 @@ public class Player extends ScrollingEntity {
 	}
 	public Player(Image i, CollisionBox c) {
 		super(i, c);
+	}
+	
+	public void setWalkSpeed(float f){
+		this.walkSpeed = f;
+	}
+	public void setCanJump(boolean canJump) {
+		this.canJump = canJump;
+	}
+	
+	public float getWalkSpeed(){
+		return walkSpeed;
+	}
+	public boolean getCanJump() {
+		return canJump;
 	}
 	
 }
