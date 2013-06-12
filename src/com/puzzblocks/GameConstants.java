dea59818d9be5413e31e3a1dfdbff9423a9cc858
Cin316 <joe13reich@gmail.com>
2013-06-12 09:00:35 -0400

    Added LEVELS_DIRECTORY constant.

--- src/com/puzzblocks/GameConstants.java
+++ src/com/puzzblocks/GameConstants.java
@@ -3,6 +3,7 @@ package com.puzzblocks;
 public class GameConstants {
 	
 	public static final String ART_DIRECTORY = "art";
+	public static final String LEVELS_DIRECTORY = "data/levels";
 	public static final int CHARACTER_WIDTH = 64;
 	public static final int CHARACTER_HEIGHT = 128;
 	public static final int TILE_WIDTH = 64;
