9674d6283815b424144bc337317d722964040112
Cin316 <joe13reich@gmail.com>
2013-06-11 21:47:40 -0400

    Updated Puzz Blocks Dev Log

--- src/com/puzzblocks/GameConstants.java
+++ src/com/puzzblocks/GameConstants.java
@@ -2,6 +2,10 @@ package com.puzzblocks;
 
 public class GameConstants {
 	
-	public final static String ART_DIRECTORY = "art";
+	public static final String ART_DIRECTORY = "art";
+	public static final int CHARACTER_WIDTH = 64;
+	public static final int CHARACTER_HEIGHT = 128;
+	public static final int TILE_WIDTH = 64;
+	public static final int TILE_HEIGHT = 64;
 	
 }
