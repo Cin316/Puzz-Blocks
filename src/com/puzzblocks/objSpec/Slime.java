f8708cfdd6f7c2871da7d6d899ec3e34f9683f87
Cin316 <joe13reich@gmail.com>
2013-06-12 08:55:55 -0400

    Added onPLayerWalk(Player) hook and new constructor to Slime class.

--- src/com/puzzblocks/objSpec/Slime.java
+++ src/com/puzzblocks/objSpec/Slime.java
@@ -2,19 +2,24 @@ package com.puzzblocks.objSpec;
 
 import java.awt.Image;
 
+import com.puzzblocks.GameConstants;
+import com.puzzblocks.Main;
+import com.puzzblocks.obj.Player;
 import com.utilis.game.obj.CollisionBox;
 import com.utilis.game.obj.Tile;
 
 public class Slime extends Tile {
 	
-	public Slime(CollisionBox c, int x, int y) {
-		super(c, x, y);
+	public static Image art = Main.getArt("Tiles/Metal Block/Slime.gif");
+	public static CollisionBox collisionBox = new CollisionBox(GameConstants.TILE_WIDTH, GameConstants.TILE_HEIGHT, true);
+	
+	public Slime(int x, int y){
+		super(art, collisionBox, x, y);
 	}
-	public Slime(int width, int height, int x, int y) {
-		super(width, height, x, y);
-	}
-	public Slime(Image i, CollisionBox c, int x, int y) {
-		super(i, c, x, y);
+	
+	public void onPlayerWalk(Player p){
+		p.setCanJump(false);
+		p.setWalkSpeed(0.4F);
 	}
 	
 }
