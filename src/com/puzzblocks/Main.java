dd529f65b0fc8d39b6b2de2dece72038267d661b
Cin316 <joe13reich@gmail.com>
2013-06-11 21:36:59 -0400

    Created getPuzzBlocksDirectory() method.

--- src/com/puzzblocks/Main.java
+++ src/com/puzzblocks/Main.java
@@ -6,12 +6,13 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLDecoder;
-
 import javax.imageio.ImageIO;
-
 public class Main {
 	
 	public static void main(String[] args) {
+		
+		PuzzBlocks game = new PuzzBlocks();
+		//game.close();
 		
 	}
 	
@@ -22,11 +23,44 @@ public class Main {
 	 */
 	public static Image getArt(String s){
 		Image i = null;
-		String mainLoc = "";
 		String fileLoc = "";
+		File root;
+		
+		//Creates file to represent the Puzz Blocks root directory folder.
+		root = new File(Main.getPuzzBlocksDirectory());
+		fileLoc = root.getAbsolutePath();
+		//Adds system equivalent of /art/ to root File;
+		fileLoc += "/";
+		fileLoc += GameConstants.ART_DIRECTORY;
+		fileLoc += "/";
+		
+		//Adds input String to filename.
+		fileLoc += s;
+		System.out.println(fileLoc);
+		//Replaces the path's "/"'s with OS specific path separators.
+		//Creates a file from the complete path
+		File image =  new File(fileLoc);
+		
+		//Reads an Image from the file.
+		try {
+			i = ImageIO.read(image);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		return i;
+	}
+	
+	/**
+	 * Gets a String representing the path of the Puzz Blocks root directory.
+	 * @return String representing path of the Puzz Blocks root directory.
+	 */
+	public static String getPuzzBlocksDirectory(){
+		String mainLoc = "";
+		URL mainLocation;
 		File bin;
 		File root;
-		URL mainLocation;
 		
 		//Gets location of Main class (without package folders) (ex /bin).
 		mainLocation = Main.class.getProtectionDomain().getCodeSource().getLocation();
@@ -42,28 +76,8 @@ public class Main {
 		bin = new File(mainLoc);
 		//Gets root PuzzBlocks directory.
 		root = bin.getParentFile();
-		fileLoc = root.getAbsolutePath();
-		//Adds system equivalent of /art/ to root File;
-		fileLoc += File.pathSeparator;
-		fileLoc += GameConstants.ART_DIRECTORY;
-		fileLoc += File.pathSeparator;
 		
-		//Adds input String to filename.
-		fileLoc += s;
-		
-		//Replaces the path's "/"'s with OS specific path separators.
-		//Creates a file from the complete path
-		File image =  new File(fileLoc);
-		
-		//Reads an Image from the file.
-		try {
-			i = ImageIO.read(image);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		
-		return i;
+		return root.getAbsolutePath();
 	}
 	
 }
