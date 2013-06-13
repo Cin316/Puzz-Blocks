3ba2ac2c7480b932bbe5f2e935cbaee612198e3d
Cin316 <joe13reich@gmail.com>
2013-06-13 18:09:48 -0400

    Created Level class.

---
+++ src/com/puzzblocks/obj/Level.java
@@ -0,0 +1,38 @@
+package com.puzzblocks.obj;
+
+public class Level {
+	
+	String name;
+	String levelUnlockName;
+	WorldCanvas world;
+	
+	public Level(String name, WorldCanvas world){
+		this.name = name;
+		this.world = world;
+	}
+	public Level(String name, String nextLevel, WorldCanvas world){
+		this.name = name;
+		this.levelUnlockName = nextLevel;
+		this.world = world;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public String getLevelUnlockName() {
+		return levelUnlockName;
+	}
+	public WorldCanvas getWorld() {
+		return world;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setLevelUnlockName(String levelUnlockName) {
+		this.levelUnlockName = levelUnlockName;
+	}
+	public void setWorld(WorldCanvas world) {
+		this.world = world;
+	}
+	
+}
