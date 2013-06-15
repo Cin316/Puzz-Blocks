dc8ea6208558b17727d97e778b6bc0023416ba12
Cin316 <joe13reich@gmail.com>
2013-06-15 13:59:31 -0400

    Added DARKENING_VALUE constant.

--- src/com/puzzblocks/GameConstants.java
+++ src/com/puzzblocks/GameConstants.java
@@ -4,9 +4,13 @@ public class GameConstants {
 	
 	public static final String ART_DIRECTORY = "art";
 	public static final String LEVELS_DIRECTORY = "data/levels";
+	
 	public static final int CHARACTER_WIDTH = 64;
 	public static final int CHARACTER_HEIGHT = 128;
+	
 	public static final int TILE_WIDTH = 64;
 	public static final int TILE_HEIGHT = 64;
+	
+	public static final int DARKENING_VALUE = 40;
 	
 }
