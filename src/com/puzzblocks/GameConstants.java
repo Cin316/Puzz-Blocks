819d707aaf835a8a11cb5424b24aad01c5b1026f
Cin316 <joe13reich@gmail.com>
2013-06-15 15:07:48 -0400

    Added backgroundTile darkening support. Adjusted DARKENING_VALUE in GameConstants.

--- src/com/puzzblocks/GameConstants.java
+++ src/com/puzzblocks/GameConstants.java
@@ -11,6 +11,6 @@ public class GameConstants {
 	public static final int TILE_WIDTH = 64;
 	public static final int TILE_HEIGHT = 64;
 	
-	public static final int DARKENING_VALUE = 40;
+	public static final int DARKENING_VALUE = 10;
 	
 }
--- src/com/puzzblocks/obj/WorldCanvas.java
+++ src/com/puzzblocks/obj/WorldCanvas.java
@@ -1,15 +1,19 @@
 package com.puzzblocks.obj;
 
-import java.awt.Graphics;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.WritableRaster;
 
-import com.puzzblocks.GameConstants;
+import com.puzzblocks.*;
 import com.utilis.game.gui.*;
 import com.utilis.game.obj.*;
 
 public class WorldCanvas extends ScrollingCanvas {
 
 	private static final long serialVersionUID = 1L;
-	public Tile backgroundTile;
+	protected Tile backgroundTile;
+	protected Image darkenedTileImage;
 	
 	public WorldCanvas(Screen s) {
 		super(s);
@@ -17,6 +21,7 @@ public class WorldCanvas extends ScrollingCanvas {
 	public WorldCanvas(Screen s, Tile t) {
 		super(s);
 		backgroundTile = t;
+		darkenedTileImage = darkenImage( Main.deepCopy( (BufferedImage) backgroundTile.getImage() ) );
 	}
 	public WorldCanvas(int xTiles, int yTiles) {
 		super(xTiles, yTiles, GameConstants.TILE_WIDTH, GameConstants.TILE_WIDTH);
@@ -24,6 +29,7 @@ public class WorldCanvas extends ScrollingCanvas {
 	public WorldCanvas(int xTiles, int yTiles, Tile t) {
 		super(xTiles, yTiles, GameConstants.TILE_WIDTH, GameConstants.TILE_WIDTH);
 		backgroundTile = t;
+		darkenedTileImage = darkenImage( Main.deepCopy( (BufferedImage) backgroundTile.getImage() ) );
 	}
 	
 	public Tile getBackgroundTile() {
@@ -31,27 +37,77 @@ public class WorldCanvas extends ScrollingCanvas {
 	}
 	public void setBackgroundTile(Tile backgroundTile) {
 		this.backgroundTile = backgroundTile;
+		darkenedTileImage = darkenImage( Main.deepCopy( (BufferedImage) backgroundTile.getImage() ) );
 	}
 	
 	public void draw(Graphics g){
 		
+		//Tile the backgroundTile if it exists.
 		if(backgroundTile != null){
+			
 			int width = getWidth();
 			int height = getHeight();
-			int imageW = backgroundTile.getImage().getWidth(this);
-			int imageH = backgroundTile.getImage().getHeight(this);
+			int imageW = darkenedTileImage.getWidth(this);
+			int imageH = darkenedTileImage.getHeight(this);
 			
 			// Tile the image to fill our area.
 			for (int x = 0; x < width; x += imageW){
 				for (int y = 0; y < height; y += imageH){
-					g.drawImage(backgroundTile.getImage(), x, y, this);
+					g.drawImage(darkenedTileImage, x, y, this);
 				}
 			}
+			
 		}
 		
 		paintTiles(g);
 		paintEntities(g);
 		paintHUD(g);
+		
+	}
+	
+	protected Image darkenImage(Image i){
+		
+		BufferedImage buff = (BufferedImage) i;
+		int rgb;
+		int darkRGB;
+		int red,green,blue;
+		
+		for(int y=0; y<buff.getHeight();y++){
+			for(int x=0; x<buff.getWidth();x++){
+				rgb = buff.getRGB(x, y);
+				Color c = new Color(rgb);
+				
+				red = c.getRed();
+				green = c.getGreen();
+				blue = c.getGreen();
+				
+				red -= GameConstants.DARKENING_VALUE;
+				green -= GameConstants.DARKENING_VALUE;
+				blue -= GameConstants.DARKENING_VALUE;
+				
+				red = rgbCap(red);
+				green = rgbCap(green);
+				blue = rgbCap(blue);
+				
+				Color newColor = new Color(red, green, blue);
+				darkRGB = newColor.getRGB();
+				
+				buff.setRGB(x, y, darkRGB);
+			}
+		}
+		
+		return buff;
+	}
+	
+	private int rgbCap(int color){
+		
+		if(color<0){
+			color = 0;
+		}else if(color>255){
+			color = 255;
+		}
+		
+		return color;
 		
 	}
 
