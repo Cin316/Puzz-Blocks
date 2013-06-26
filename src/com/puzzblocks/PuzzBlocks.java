119df305d935f36d59415f6b92c1df2fae1dbdc0
Cin316 <joe13reich@gmail.com>
2013-06-25 21:49:20 -0400

    Added animate method with dynamic FPS in PuzzBlocks class.

--- src/com/puzzblocks/PuzzBlocks.java
+++ src/com/puzzblocks/PuzzBlocks.java
@@ -2,6 +2,7 @@ package com.puzzblocks;
 
 import com.puzzblocks.LevelLoader.InvalidLevelFormatException;
 import com.puzzblocks.obj.*;
+import com.utilis.Utilis;
 import com.utilis.game.gui.Window;
 
 /**
@@ -13,17 +14,60 @@ public class PuzzBlocks {
 	WorldCanvas currentWorld;
 	Window loadedLevelWindow;
 	Level level;
+	private long desiredFPS = 60;
+	private long desiredDeltaLoop = (1000*1000*1000)/desiredFPS;
+	boolean running = true;
 	
 	public PuzzBlocks(){
 		
 		try {
-			level = LevelLoader.loadLevel("Cool Art");
+			level = LevelLoader.loadLevel("1-1");
 		} catch (InvalidLevelFormatException e) {
 			e.printStackTrace();
 		}
 		
 		loadedLevelWindow =  new Window(level.getWorld());
 		loadedLevelWindow.setVisible(true);
+		
+		animate();
+		
+	}
+	
+	public void animate(){
+		
+		long beginLoopTime;
+		long endLoopTime;
+		long deltaLoop;
+		long lastSecond = 0;
+		int fps = 0;
+		
+		while(running){
+			beginLoopTime = System.nanoTime();
+			
+			loadedLevelWindow.repaint();
+			fps++;
+			
+			//Calculate fps.
+			if(beginLoopTime >= lastSecond + 1000000000){
+				
+				System.out.println(fps);
+				fps = 0;
+				lastSecond = beginLoopTime;
+			}
+			
+			endLoopTime = System.nanoTime();
+			deltaLoop = endLoopTime - beginLoopTime;
+			
+				if(deltaLoop > desiredDeltaLoop){
+					//Do nothing. We are already late!
+				}else{
+					try{
+						Thread.sleep((desiredDeltaLoop - deltaLoop)/(1000*1000));
+					}catch(InterruptedException e){
+						//Do nothing.
+					}
+				}
+		}
 		
 	}
 	
@@ -33,6 +77,7 @@ public class PuzzBlocks {
 	public void close(){
 		
 		loadedLevelWindow.setVisible(false);
+		running = false;
 		System.exit(0);
 		
 	}
