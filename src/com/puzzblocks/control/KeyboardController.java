33d29c18369dd13c88ca120e12658cf01680071b
Cin316 <joe13reich@gmail.com>
2013-06-27 20:47:36 -0400

    Added implementation of KeyboardController.

--- src/com/puzzblocks/control/KeyboardController.java
+++ src/com/puzzblocks/control/KeyboardController.java
@@ -1,11 +1,38 @@
 package com.puzzblocks.control;
 
 import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import com.puzzblocks.GameConstants;
+import com.puzzblocks.Physics;
+import com.puzzblocks.PuzzBlocks;
 
 public class KeyboardController extends KeyAdapter {
-
-	public KeyboardController() {
-		
+	
+	protected PuzzBlocks game;
+	
+	public KeyboardController(PuzzBlocks pb) {
+		game = pb;
+		if(GameConstants.DEBUG_MODE){
+			System.out.println("[KEYBOARD] KeyboardController was successfully created.");
+		}
+	}
+	
+	public void keyPressed(KeyEvent e){
+		if(e.getKeyCode()==GameConstants.KEY_LEFT){
+			Physics.setMovingLeft(true);
+		}else if(e.getKeyCode()==GameConstants.KEY_RIGHT){
+			Physics.setMovingRight(true);
+		}else if(e.getKeyCode()==GameConstants.KEY_JUMP){
+			Physics.setJumping(true);
+		}
+	}
+	public void keyReleased(KeyEvent e){
+		if(e.getKeyCode()==GameConstants.KEY_LEFT){
+			Physics.setMovingLeft(false);
+		}else if(e.getKeyCode()==GameConstants.KEY_RIGHT){
+			Physics.setMovingRight(false);
+		}
 	}
 
 }
